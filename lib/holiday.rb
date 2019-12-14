cb3b3a3279a488176da7885bc381ed1712b35526
Tom Banish <tombanish@gmail.com>
2019-12-14 15:25:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,8 +78,8 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   
-  holiday_hash.each do |season, holiday|
-    
+  holiday_hash.map do |season, holiday|
+    holiday.map do ||
 
 end
 
