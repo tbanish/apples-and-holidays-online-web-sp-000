6122e999382903bc5c5704dd774993b69c77bc26
Tom Banish <tombanish@gmail.com>
2019-12-14 15:31:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,7 +81,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holidays|
     holidays.map do |holiday, supplies|
       if supplies.include?("BBQ")
-        array << holiday
+        bbq_array << holiday
       end
     end
   end
