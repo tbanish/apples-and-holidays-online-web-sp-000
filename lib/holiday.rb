9b681db8e22af396236d9ec899e9df92d7ed1f2c
Tom Banish <tombanish@gmail.com>
2019-12-14 15:53:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.to_s.capitalize}:"
     holidays.each do |holiday, supply|
-      puts "  #{holiday.to_s.split(" ").map {|w| w.capitalize!}.join(" ")}: #{supply.join(", ")}"
+      puts "  #{holiday.to_s.split("_").map {|w| w.capitalize!}.join(" ")}: #{supply.join(", ")}"
     end
   end
 end
