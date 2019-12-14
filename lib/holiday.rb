4f48a95ca603a2701e1a2a69dabec93281268422
Tom Banish <tombanish@gmail.com>
2019-12-14 14:50:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,11 +58,7 @@ def all_winter_holiday_supplies(holiday_hash)
   winter_supplies = []
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
-      if holiday = :christmas || holiday = :new_years
-        winter_supplies << supplies
-      end
-      winter_supplies.flatten
-      binding.pry
+      winter_supplies << supplies if holiday = :christmas || holiday = :new_years
     end
   end
 end
