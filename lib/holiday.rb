b61ece4f2005e29e398ca40bfee1991c36023c36
Tom Banish <tombanish@gmail.com>
2019-12-14 14:27:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,6 +57,7 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   winter_supplies = []
   holiday_hash.each do |season|
+    binding.pry
   end
 end
 
