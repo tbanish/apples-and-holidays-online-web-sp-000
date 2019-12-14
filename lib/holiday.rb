dd89cd9edd3f93dcbd6311d26d4f5456b86e17aa
Tom Banish <tombanish@gmail.com>
2019-12-14 15:34:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,9 +70,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   
-  holiday_hash.each do |season, holiday|
+  holiday_hash.each do |season, holidays|
     puts "#{season.capitalize}:"
-    binding.pry
+    
   end
 
 end
